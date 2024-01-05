6ff460ea4dd1f306907747b953602c4c54208e43
James Jefferies <james@Jamess-MacBook-Pro.local>
2024-01-05 15:40:43 +0000

    added login endpoint

--- resources/user.py
+++ resources/user.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
 
 from db import db
 from models import UserModel
@@ -19,16 +20,27 @@ class UserRegister(MethodView):
             abort(409, message="A user with that username already exists.")
 
         user = UserModel(
-            username = user_data["username"],
-            # password = user_data["password"]
-            password = pbkdf2_sha256.hash(user_data["password"])
+            username=user_data["username"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
         )
-        db.sesson.add(user)
-        db.sesson.commit()
-        return {"message": "User created sucessfully."}, 201
+        db.session.add(user)
+        db.session.commit()
+
+        return {"message": "User created successfully."}, 201
+
+@blp.route("/login")
+class login(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}
+        
+        abort(401, message="Invalid credentials")
 
 
-    
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
     @blp.response(200, UserSchema)
