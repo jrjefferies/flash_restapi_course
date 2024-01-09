7d8256b07b3451154b9bef28c6e07ada57289151
James Jefferies <james@Jamess-MacBook-Pro.local>
2024-01-09 14:12:39 +0000

    migrations

--- models/item.py
+++ models/item.py
@@ -5,6 +5,7 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)   #nullable = can it be Null (empty), False means it must exists
+    description = db.Column(db.String(240), nullable=True)
     price = db.Column(db.Integer, unique=True, nullable=False) 
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=True, nullable=False) 
     store = db.relationship("StoreModel", back_populates="item")
