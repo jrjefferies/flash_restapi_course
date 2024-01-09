3947d0ec356103bf7eab8a89b94b402476d8e3b2
James Jefferies <james@Jamess-MacBook-Pro.local>
2024-01-09 13:00:29 +0000

    begining of migrate

--- resources/item.py
+++ resources/item.py
@@ -1,8 +1,8 @@
-import uuid
-from flask import request
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from flask_jwt_extended import jwt_required
 
 from db import db
 from models import ItemModel
@@ -13,14 +13,18 @@ blp = Blueprint("items", __name__, description="Operations on items")
 
 @blp.route("/item/<int:item_id>")      # http://127.0.0.1:5000/item/ITEM_ID
 class Item(MethodView):
+    @jwt_required()            #means the flask need a JWT token in the auth header
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
     
+    @jwt_required()
     @blp.arguments(ItemUpdateSchema)              #Order of decorators matters, in this case the arguments comes first the response later
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):   #Note arguments marshmallow data comes before the routes data
+        
+        
         item = ItemModel.query.get(item_id)
         if item:     # idempotent (will create new item)
             item.price = item_data["price"]
@@ -33,6 +37,7 @@ class Item(MethodView):
 
         return item
     
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
 
@@ -48,6 +53,7 @@ class ItemList(MethodView):
     def get(self):
         return ItemModel.query.all()
 
+    @jwt_required(fresh=True)     #fresh token required
     @blp.arguments(ItemSchema)
     @blp.response(200, ItemSchema)
     def post(self,item_data):
@@ -56,8 +62,8 @@ class ItemList(MethodView):
         try: 
             db.session.add(item)
             db.session.commit()
-        except SQLAlchemyError:
-            abort(500, message="An error occured while inserting the item.")
+        except SQLAlchemyError as e:
+            abort(500, message=f"An error occured while inserting the item. {e}")
 
         return item
 
