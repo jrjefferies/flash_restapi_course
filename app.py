50ef2833b46c47524f456f7073cab91b168f09bb
James Jefferies <james@Jamess-MacBook-Pro.local>
2024-01-10 16:09:35 +0000

    remove db.create_all

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def create_app(db_url=None):
     # def create_tables():
     #     db.create_all()
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
